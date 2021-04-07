8f413e332209871f14e5339dbc3013df8a7401c0
Gabriele Alessi <89archaon89@gmail.com>
2021-04-07 15:29:52 +0200

    carusel funzionante

--- js/script.js
+++ js/script.js
@@ -3,11 +3,25 @@ function init() {
         el: '#app',
 
         data:{
-
+            'imgPath': "img/pic-",
+            'imgNum': 1
         },
 
         methods:{
-            
+            prev: function () {
+                if (this.imgNum == 0) {
+                    this.imgNum = 2
+                }else {
+                    this.imgNum --
+                }
+            },
+            next: function () {
+                if (this.imgNum == 2) {
+                    this.imgNum = 0
+                }else {
+                    this.imgNum ++
+                }
+            }
         }
     });
 }
